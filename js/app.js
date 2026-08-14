5b754a1405c4a66b9c3e79b42709e41e493d3a1e
ngomez0812 <ngomez@xpertasesores.com.mx>
2026-08-14 10:06:58 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://benefits-affects-rugby-vertex.trycloudflare.com";
+const API_BASE_URL = "https://intelligence-weighted-raising-springs.trycloudflare.com";
 
 const loginView = document.getElementById("loginView");
 const appView = document.getElementById("appView");
