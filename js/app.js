b9bad27bbc6c62fe0fd42b7982386d57e4ea7c3d
ngomez0812 <ngomez@xpertasesores.com.mx>
2026-08-28 09:43:00 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://axis-ranked-cloud-dance.trycloudflare.com";
+const API_BASE_URL = "https://favourite-dude-cad-raleigh.trycloudflare.com";
 
 const loginView = document.getElementById("loginView");
 const appView = document.getElementById("appView");
