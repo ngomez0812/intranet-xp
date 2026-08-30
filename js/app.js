45010f034dc09b051d3b5c8ba46df0ad9c9409b1
ngomez0812 <ngomez@xpertasesores.com.mx>
2026-08-30 14:18:02 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://touring-preceding-ceremony-motels.trycloudflare.com";
+const API_BASE_URL = "https://mainstream-seating-santa-talking.trycloudflare.com";
 
 const loginView = document.getElementById("loginView");
 const appView = document.getElementById("appView");
